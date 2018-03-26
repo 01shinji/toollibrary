5ebedb4c0773ee555cc2522123dee009b3683a1d
01shinji <satoyama.shinji@gmail.com>
2018-03-26 11:44:05 +0900

    production.rb訂正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -105,7 +105,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       user_name: ENV['SENDGRID_USERNAME'],
-  　　 password: ENV['SENDGRID_PASSWORD'],
+      password: ENV['SENDGRID_PASSWORD'],
       domain: "heroku.com",
       address: "smtp.sendgrid.net",
       port: 587,
