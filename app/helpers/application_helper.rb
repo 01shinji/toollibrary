beed05bc3741b88d54f807e7dfe64b65479cbaa9
01shinji <satoyama.shinji@gmail.com>
2018-03-29 16:19:47 +0900

    herokuでのactioncable不具合解消

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  def stripe_express_path    
-    "https://connect.stripe.com/express/oauth/authorize?response_type=code&client_id=ca_CFtYh8UW9pMtOBA6WGsQCqFyFwxOEKol&scope=read_write"
+  def stripe_express_path
+    "https://connect.stripe.com/express/oauth/authorize?response_type=code&client_id=ca_CFtY2gH6iAeDCdShvLTkvNdES53P1JAA&scope=read_write"
   end
 end
