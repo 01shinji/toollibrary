11b16dfd3a3e28ef221111bc0b8ec0b6bfa6a4b2
01shinji <satoyama.shinji@gmail.com>
2018-05-15 09:18:46 +0900

    listing出品エラー修正2

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -23,26 +23,29 @@ class ListingsController < ApplicationController
       flash[:alert] = "電話番号の認証が必要です"
       return redirect_to edit_user_registration_path
 
-    elsif current_user.license_file_name.blank?
+    elsif !current_user.license_file_name
       flash[:alert] = "身分証明書の登録が必要です"
       return redirect_to edit_user_registration_path
 
-    else
-      current_user.nickname.blank? ||
-      current_user.image_file_name.blank? ||
-      current_user.address_zipcode.blank? ||
-      current_user.address_prefecture_name.blank?
+    elsif
+      !current_user.nickname ||
+      !current_user.image_file_name ||
+      !current_user.address_zipcode ||
+      !current_user.address_prefecture_name
 
       return redirect_to edit_user_registration_path, alert: "プロフィールの登録をお願いします"
-    end
+
+    else
 
     @listing = current_user.listings.build(listing_params)
+
     @photos = @listing.photos
-    if @listing.save
-      redirect_to information1_listing_path(@listing), notice: "商品の仮登録が成功しました!最後まで入力をお願いします〜"
-    else
-      flash[:alert] = "出品がうまくいきませんでした..."
+      if @listing.save
+       redirect_to information1_listing_path(@listing), notice: "商品の仮登録が成功しました!最後まで入力をお願いします〜"
+      else
+       flash[:alert] = "出品がうまくいきませんでした..."
       render :new
+      end
 
     end
   end
