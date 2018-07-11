6c7956c650d37eade6e09c6491090962fbead1f7
01shinji <satoyama.shinji@gmail.com>
2018-07-11 10:21:11 +0900

    listing.showのdescription修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,18 @@ module ApplicationHelper
   def stripe_express_path
     "https://connect.stripe.com/express/oauth/authorize?response_type=code&client_id=ca_CFtY2gH6iAeDCdShvLTkvNdES53P1JAA&scope=read_write"
   end
+
+  require "uri"
+
+  def text_url_to_link text
+    URI.extract(text, ['http','https']).uniq.each do |url|
+      sub_text = ""
+      sub_text << "<a href=" << url << " target=\"_blank\">" << url << "</a>"
+
+      text.gsub!(url, sub_text)
+    end
+
+    return text
+  end
+
 end
