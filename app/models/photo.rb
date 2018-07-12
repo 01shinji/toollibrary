f4a328676c9c9bbedc37013781dd9fc551817a1a
01shinji <satoyama.shinji@gmail.com>
2018-07-12 15:18:46 +0900

    auto-orientエラー実験

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,11 +3,11 @@ class Photo < ApplicationRecord
 
 
 
-  has_attached_file :image, styles: { medium: "300x300#",  thumb: "100x100#" },  convert_options: { all: "-auto-orient" }
+  has_attached_file :image, styles: { medium: "300x300#",  thumb: "100x100#" }
 
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
-  
+
 
 
 
